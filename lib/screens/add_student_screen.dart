1c8516b5543221cf4b12d705c2635070c2f70be9
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-07 22:55:45 +0300

    stl setup

---
+++ lib/screens/add_student_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddStudentScreen extends StatelessWidget {
+  const AddStudentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
